c9bbd5608d78deab20f05fb73bddf0e52979e3a9
meikuam <meikuam@gmail.com>
2018-02-12 15:01:44 +0700

    Pooling layer Backward copy top data to all positions of bottom data.

--- src/danknet/layers/pooling_layer.cpp
+++ src/danknet/layers/pooling_layer.cpp
@@ -22,9 +22,9 @@ PoolingLayer<Dtype>::PoolingLayer(int kernel_w, int kernel_h,
 
       //-------------create top vector--------------
       //(input_dim + 2 * pad - kernel_size) / stride + 1;
-      int out_w = (this->bottom_[0]->shape().width() + /*2 **/ pad_w_ - kernel_w_) / stride_w_ + 1;
-      int out_h = (this->bottom_[0]->shape().height() + /*2 **/ pad_h_ - kernel_h_) / stride_h_ + 1;
-      this->top_.push_back(new Blob<Dtype>(this->name_ + "_data", Shape(out_w, out_h, this->bottom_[0]->shape().depth(), this->bottom_[0]->shape().batch())));
+      int top_w = (this->bottom_[0]->shape().width() + /*2 **/ pad_w_ - kernel_w_) / stride_w_ + 1;
+      int top_h = (this->bottom_[0]->shape().height() + /*2 **/ pad_h_ - kernel_h_) / stride_h_ + 1;
+      this->top_.push_back(new Blob<Dtype>(this->name_ + "_data", Shape(top_w, top_h, this->bottom_[0]->shape().depth(), this->bottom_[0]->shape().batch())));
       top = this->top_;
 
   }
@@ -40,18 +40,18 @@ PoolingLayer<Dtype>::Forward() {
         Data3d<Dtype>* top_data = top->Data(batch);
         Shape top_shape = top_data->shape();
         for(int depth = 0; depth < top_shape.depth(); depth++) {
-            for(int out_x = 0, inp_x = 0/*- pad_w_*/; out_x < top_shape.width(); out_x++,  inp_x += stride_w_ ) {
-                for(int out_y = 0, inp_y = 0/*- pad_h_*/; out_y < top_shape.height(); out_y++, inp_y += stride_h_) {
+            for(int top_x = 0, bottom_x = 0/*- pad_w_*/; top_x < top_shape.width(); top_x++,  bottom_x += stride_w_ ) {
+                for(int top_y = 0, bottom_y = 0/*- pad_h_*/; top_y < top_shape.height(); top_y++, bottom_y += stride_h_) {
                     Dtype val = numeric_limits<Dtype>::min();
                     //--------max pooling-----------
                     for(int x = 0; x < kernel_w_; x++) {
                         for(int y = 0; y < kernel_h_; y++) {
-                            if(*bottom_data->data(inp_x + x, inp_y + y, depth) > val) {
-                                val = *bottom_data->data(inp_x + x, inp_y + y, depth);
+                            if(*bottom_data->data(bottom_x + x, bottom_y + y, depth) > val) {
+                                val = *bottom_data->data(bottom_x + x, bottom_y + y, depth);
                             }
                         }
                     }
-                    *top_data->data(out_x, out_y, depth) = val;
+                    *top_data->data(top_x, top_y, depth) = val;
                 }
             }
         }
@@ -70,25 +70,16 @@ PoolingLayer<Dtype>::Backward() {
         Data3d<Dtype>* bottom_data = bottom->Data(batch);
         Data3d<Dtype>* top_data = top->Data(batch);
         Shape top_shape = top_data->shape();
+        //-------------------depth--------------------
         for(int depth = 0; depth < top_shape.depth(); depth++) {
-            for(int out_x = 0, inp_x = 0/*- pad_w_*/; out_x < top_shape.width(); out_x++,  inp_x += stride_w_ ) {
-                for(int out_y = 0, inp_y = 0/*- pad_h_*/; out_y < top_shape.height(); out_y++, inp_y += stride_h_) {
-                    Dtype val = numeric_limits<Dtype>::min();
-                    //--------max pooling-----------
+            //-----------------top_shape------------------
+            for(int bottom_x = 0, top_x = 0/*- pad_w_*/; top_x < top_shape.width(); top_x++,  bottom_x += stride_w_ ) {
+                for(int bottom_y = 0, top_y = 0/*- pad_h_*/; top_y < top_shape.height(); top_y++, bottom_y += stride_h_) {
+                    Dtype val = *top_data->data(top_x, top_y, depth);
+                    //-------------------kernel-------------------
                     for(int x = 0; x < kernel_w_; x++) {
                         for(int y = 0; y < kernel_h_; y++) {
-                            if(*bottom_data->data(inp_x + x, inp_y + y, depth) > val) {
-                                val = *bottom_data->data(inp_x + x, inp_y + y, depth);
-                            }
-                        }
-                    }
-                    for(int x = 0; x < kernel_w_; x++) {
-                        for(int y = 0; y < kernel_h_; y++) {
-                            *bottom_data->data(inp_x + x, inp_y + y, depth) = *top_data->data(inp_x + x, inp_y + y, depth);
-//                            if(*bottom_data->data(inp_x + x, inp_y + y, depth) != val) {
-//                                *bottom_data->data(inp_x + x, inp_y + y, depth) = 0;
-//                            } else {
-//                            }
+                            *bottom_data->data(bottom_x + x, bottom_y + y, depth) = val;
                         }
                     }
                 }
